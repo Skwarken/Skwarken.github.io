4a42c958ac99fff71ac51f8f33bc3cd432431627
Skwarken <skwarken5@gmail.com>
2024-04-29 09:51:51 +0200

    Create Index.php

---
+++ Index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="de">
+<head>
+    <title>Discord-bot</title>
+
+    <!-- Recommended meta tags -->
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+    <!-- PyScript CSS -->
+    <link rel="stylesheet" href="https://pyscript.net/releases/2024.4.2/core.css">
+
+    <!-- This script tag bootstraps PyScript -->
+    <script type="module" src="https://pyscript.net/releases/2024.4.2/core.js"></script>
+</head>
+<body>
+    <h1>test</h1>
+    <script type="py" src="./Dis.py" config="./pyscript.toml" terminal></script>
+</body>
+</html>
